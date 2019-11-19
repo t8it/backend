6b19c3c6e4cac0cfbefad1dad5a3fe4a5ad2eba6
BEBARBARA <cerezo.barbara@gmail.com>
2019-11-19 01:48:10 -0300

    usuarios final okey

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) =>{
 
-    res.send('Hello Tomás in Node :) ');
+    res.send('Hello in Node :) ');
 });
 
 router.get('/about', (req, res) =>{
