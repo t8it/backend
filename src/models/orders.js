edb1124225373233c61044dbb756adc795220721
BEBARBARA <cerezo.barbara@gmail.com>
2019-11-20 00:49:30 -0300

    modelo de la orden okey

--- src/models/orders.js
+++ src/models/orders.js
@@ -5,21 +5,23 @@ const OrderSchema = new Schema ({
 
          //idOrder: { type: ObjectId, reqired: true},
          idUsername: { type: String, reqired: true},
-         idShops: { type: String, reqired: true},
-         dateEntry: { type: String, reqired: true},
-         dateExit: { type: String, reqired: true},
+         idProduct: { type: String, reqired: true},
+         dateEntry: { type: Date},
+         dateExit: { type: Date},
          //productList: { type: Array<cant  {type: Integer, reqired: true},
          //                           price {type: Integer, reqired: true},
          //                           descrip{type String, reqired: true,}
          //                           >, reqired: true},
-         email: { type: String, reqired: true},
-         status: { type: String, reqired: true},
+         status: { type: String, default: "Sin retirar"},
          price: { type: Integer, reqired: true},
          cant: { type: Integer, reqired: true},
          total: { type: Integer, reqired: true}
 
 
    
-})
+},
+{
+    timestamps: true
+});
 
 module.exports= mongoose.model('Order', UserSchema);
