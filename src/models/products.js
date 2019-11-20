0727914b7e4939c14b564931d6aba4747723cfa1
BEBARBARA <cerezo.barbara@gmail.com>
2019-11-20 00:42:20 -0300

    cambio del titulo del producto

--- src/models/products.js
+++ src/models/products.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const ProductSchema = new Schema ({
-    productname: {
+    title: {
         type: String,
         required: true,
         trim: true},
